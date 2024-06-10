713907015bc0910a72941f9d6f58476ff68c8ad9
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2024-06-10 12:45:39 +0100

    Adapt to more common Elixir practices
    - use phx.gen.schema
    - use an Ecto changeset

--- lib/phoenix_example/blog/comment.ex
+++ lib/phoenix_example/blog/comment.ex
@@ -1,28 +1,34 @@
 defmodule PhoenixExample.Comment do
+  alias PhoenixExample.Comment, as: Comment
   alias PhoenixExample.Repo, as: Repo
+
   use Ecto.Schema
 
+  import Ecto.Changeset
   import Ecto.Query, only: [from: 2]
 
   schema "comment" do
     field :author, :string
     field :comment, :string
-    timestamps()
+
+    timestamps(type: :utc_datetime)
   end
 
   def all() do
-    query =
-      from comment in "comment",
-        select: [:inserted_at, :author, :comment],
-        order_by: [desc: :inserted_at]
+    query = from Comment, order_by: [desc: :inserted_at]
 
     Repo.all(query)
   end
 
-  def insert(comment, author) do
-    Repo.insert!(%PhoenixExample.Comment{
-      comment: comment,
-      author: author
-    })
+  def insert(attrs) do
+    %Comment{}
+    |> Comment.changeset(attrs)
+    |> Repo.insert!()
+  end
+
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:author, :comment])
+    |> validate_required([:author, :comment])
   end
 end
